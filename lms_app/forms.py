b800be07961cbef9d221f8639d88fff4a9270d71
Islam412 <islam.124.hamdy@gmail.com>
2023-08-22 06:34:23 +0300

    work catogry in index

--- lms_app/forms.py
+++ lms_app/forms.py
@@ -22,6 +22,17 @@ class BookForm(forms.ModelForm):
         ]
     widgets = {
         'title' : forms.TextInput(attrs={'class':'from-control'}),
+        'author' : forms.TextInput(attrs={'class':'from-control'}),
+        'photo_book' : forms.FileInput(attrs={'class':'from-control'}),
+        'photo_author' : forms.FileInput(attrs={'class':'from-control'}),
+        'pages' : forms.NumberInput(attrs={'class':'from-control'}),
+        'price' : forms.NumberInput(attrs={'class':'from-control'}),
+        'retal_price_day' : forms.NumberInput(attrs={'class':'from-control'}),
+        'retal_period' : forms.NumberInput(attrs={'class':'from-control'}),
+        'active' : forms.TextInput(attrs={'class':'from-control'}),
+        'status' : forms.Select(attrs={'class':'from-control'}),
+        'category' : forms.Select(attrs={'class':'from-control'}),
+        
     }    
 
 
