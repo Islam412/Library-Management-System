d3d9ddae51b05dd0eeda2a34e8de4bfa47439cf6
Islam412 <islam.124.hamdy@gmail.com>
2023-08-22 16:46:04 +0300

    edite books bage

--- lms_app/views.py
+++ lms_app/views.py
@@ -49,8 +49,8 @@ def books(requset):
     
     context = {
         'category' : Category.objects.all(),
-        'books' : search,
-        'category' : Category.objects.all(),
+        'books': Book.objects.all(),
+        'form' : BookForm(),
         'forms' : CategoryForm(),
     }
     return render(requset,'pages/books.html', context)
